a6f5d67a69fc9539da103c66c64527bc002efaa8
thomaslapOPCR <114487960+thomaslapOPCR@users.noreply.github.com>
2023-06-28 16:00:05 +0200

    [UPDATE] DropDown en cours de creation

--- src/Pages/about/About.jsx
+++ src/Pages/about/About.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import style from "./About.module.css"
 import Banner from "../../components/Banner/Banner.jsx";
 import Image from "../../assets/images/kalen-emsley-Bkci_8qcdvQ-unsplash 2.png";
+import Dropdown from "../../components/Dropdown/Dropdown.jsx";
 
 function About() {
 
 
   return (
-    <Banner imageSrc={Image}/>
-        
+    <div className={style.Container}>
+      <Banner imageSrc={Image}/>
+
+      <Dropdown/>
+    </div>
   )
 }
 
--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as Chevron } from "./chevron-down.svg";
+import style from "./Dropdown.module.css";
 
-const Dropdown = () => {
+const Dropdown = ({ title = "test", value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div>
-            
-    </div>
+    <section className={style.DropdownBase}>
+      <button className={style.DropdownHeader} onClick={handleToggle}>
+        <h1 className={style.title}>{title}</h1>
+        <Chevron className={isOpen ? style.rotate : ""} />
+      </button>
+
+      {isOpen && (
+        <div className={style.wrapper}>
+          {Array.isArray(value) ? (
+            <ul>
+              {value.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>{value}</p>
+          )}
+        </div>
+      )}
+    </section>
   );
 };
 
